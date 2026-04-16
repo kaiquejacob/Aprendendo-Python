0b96f1b4d807322072ed5345971015befba7b045
Kaíque Jacob <kaiquejacob975@gmail.com>
2026-04-16 00:15:23 -0300

    Abrindo arquivo texto

---
+++ python/abrindo_arquivo_texto.py
@@ -0,0 +1,6 @@
+with open("arquivo_texto.txt", mode="r", encoding="UTF-8") as arquivo:
+    conteudo = arquivo.read()
+    #conteudo = arquivo.readline()
+    #conteudo = arquivo.read().splitlines()
+print(conteudo)
+
